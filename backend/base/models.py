86f2e095f69cab13e2a973e8af689f61cbda8328
sepide zn <sepid.zn@gmail.com>
2023-08-27 16:52:20 +0330

    portfolio added

--- backend/base/models.py
+++ backend/base/models.py
@@ -358,8 +358,16 @@ class Language(models.Model):
 
 
 
+class Portfolio(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100)
+    image = models.ImageField(null=True, blank=True)
+    description  = models.TextField(max_length=1000, null=True , blank=True)
 
 
+    
+def __str__(self):
+        return f'{self.employee.user.first_name} {self.employee.user.first_name}  portfolio image {self.id}'
 
 
 
